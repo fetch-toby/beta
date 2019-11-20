d345463728993b6250403f3190527a62577f2e34
root <root@ip-172-31-38-209.eu-north-1.compute.internal>
2019-11-20 20:55:30 +0000

    edited run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 sudo sar -o SAR_datafile 1 1500 >/dev/null 2>&1 &
-sudo docker run -it -v my-vol:/data1 fio-test /data1
+sudo docker run -it -v my-vol/data1:/data fio-test
 sleep 120
-sudo docker run -it -v my-vol:/data2 fio-test /data2
+sudo docker run -it -v my-vol/data2:/data fio-test
 sleep 120
-sudo docker run -it -v my-vol:/data3 fio-test /data3
+sudo docker run -it -v my-vol/data3:/data fio-test
 sleep 120
-sudo docker run -it -v my-vol:/data4 fio-test /data4
+sudo docker run -it -v my-vol/data4:/data fio-test
 sleep 120
-sudo docker run -it -v my-vol:/data5 fio-test /data5
+sudo docker run -it -v my-vol/data5:/data fio-test

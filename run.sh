5260c982696b6a28c76cb7007ca5bc99c179303b
root <root@ip-172-31-39-170.eu-north-1.compute.internal>
2019-11-23 16:57:29 +0000

    bind mount in run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 sudo sar -o SAR_datafile 1 1500 >/dev/null 2>&1 &
-sudo docker run -v my-vol1:/data fio-test
+sudo docker run -d --mount type=bind,source='/yolo/vol-1',target=/data saye16/fio-load
 sleep 120
-sudo docker run -v my-vol2:/data fio-test
+sudo docker run -d --mount type=bind,source='/yolo/vol-2',target=/data saye16/fio-load
 sleep 120
-sudo docker run -v my-vol3:/data fio-test
+sudo docker run -d --mount type=bind,source='/yolo/vol-3',target=/data saye16/fio-load
 sleep 120
-sudo docker run -v my-vol4:/data fio-test
+sudo docker run -d --mount type=bind,source='/yolo/vol-4',target=/data saye16/fio-load
 sleep 120
-sudo docker run -v my-vol5:/data fio-test
+sudo docker run -d --mount type=bind,source='/yolo/vol-5',target=/data saye16/fio-load
